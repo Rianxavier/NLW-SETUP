d9e50ab4e448c723b0913bcd67f4d43bd171dbd4
Rian Xavier <rianalvesxvr@gmail.com>
2023-01-20 17:21:24 -0300

    Saving changes

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const form = document.querySelector("form")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-20", "01-21", "01-22", "01-23"],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso ❌")
+    return
+  }
+
+  nlwSetup.addDay(today)
 }
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
